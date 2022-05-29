c84d9e8719a6430ab34853fca6d05bf0a1d6440f
optiX <55358375+traneric89@users.noreply.github.com>
2022-05-29 14:37:45 -0400

    update projects

---
+++ src/Components/Firetracker.js
@@ -0,0 +1,81 @@
+import React from "react";
+import { BsGithub } from "react-icons/bs";
+import { FiExternalLink } from "react-icons/fi";
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { useAnimation } from "framer-motion";
+
+const Weather = () => {
+  const { ref, inView } = useInView({
+    threshhold: 0.2,
+  });
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (inView) {
+      animation.start({
+        x: 0.001,
+        transition: {
+          type: "spring",
+          duration: 1.5,
+          bounce: 0.2,
+        },
+      });
+    }
+    if (!inView) {
+      animation.start({
+        x: "-100vw",
+      });
+    }
+  }, [inView]);
+
+  return (
+    <div ref={ref}>
+      <motion.div
+        animate={animation}
+        className="bg-white p-12 rounded-3xl shadow-xl shadow-gray-400 mb-20"
+      >
+        <div>
+          <img
+            src={require("../images/firetracker_ss.png")}
+            alt="Global Wildfire Tracker"
+            className="rounded-xl"
+          />
+        </div>
+        <div className="text-right mt-2">
+          <a href="https://github.com/traneric89/Weather-App" target="_blank">
+            <BsGithub className="inline w-5 h-5 mr-2 cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-105" />
+          </a>
+          <a href="https://traneric89.github.io/Weather-App/" target="_blank">
+            <FiExternalLink className="inline w-5 h-5 cursor-pointer transition ease-in-out hover:-translate-y-1 hover:scale-105" />
+          </a>
+        </div>
+        <div className="px-0 text-center">
+          <h2 className="mb-3 text-2xl md:text-3xl tracking-wide">
+            Global Wildfire Tracker
+          </h2>
+          <div className="flex justify-center my-5">
+            <span className="bg-blue-500 text-white p-1.5 rounded-2xl text-sm mx-2">
+              React
+            </span>
+            <span className="bg-blue-500 text-white p-1.5 rounded-2xl text-sm mx-2">
+              NASA API
+            </span>
+            <span className="bg-blue-500 text-white p-1.5 rounded-2xl text-sm mx-2">
+              Google Maps API
+            </span>
+          </div>
+          <div className=" w-3/4 mx-auto">
+            <p className="leading-8 text-lg">
+              React application that displays active wildfires all around the
+              world.
+            </p>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Weather;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -3,12 +3,13 @@ import Weather from "./Weather";
 import Store from "./Store";
 import MemoryGame from "./MemoryGame";
 import FEMG from "./FEMG";
+import Firetracker from "./Firetracker";
 
 const Projects = () => {
   return (
     <div className="w-4/5 mx-auto mt-32 2xl:px-32 overflow-x-hidden">
       <h1 className="text-5xl mb-20 tracking-wide font-playfair">Projects</h1>
-      <Weather />
+      <Firetracker />
       <Store />
       <MemoryGame />
       <FEMG />
